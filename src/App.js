94206e516aabf2a04941244ea5b9bb45eab89a47
Enes Eren <thenesern@gmail.com>
2022-08-22 12:11:55 +0300

    Added Code Splitting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Server-SideRendering/Home";
 import About from "./components/Server-SideRendering/About";
@@ -8,6 +8,7 @@ import ContextApp from "./components/StateManagement/Context/ContextApp";
 import RecoilApp from "./components/StateManagement/Recoil/RecoilApp";
 import ReduxApp from "./components/StateManagement/Redux/ReduxApp";
 import MobXApp from "./components/StateManagement/MobX/MobXApp";
+const AboutApp = lazy(() => import("./components/CodeSplitting/About"));
 
 function App() {
   return (
@@ -23,18 +24,22 @@ function App() {
           <Link to="/counter-recoil">Counter Recoil</Link>
           <Link to="/counter-redux">Counter Redux</Link>
           <Link to="/counter-mobx">Counter MobX</Link>
+          <Link to="/code-splitting">About App</Link>
         </li>
       </ul>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/articles" element={<Articles />} />
-        <Route exact path="/counter" element={<StateApp />} />
-        <Route exact path="/counter-context" element={<ContextApp />} />
-        <Route exact path="/counter-recoil" element={<RecoilApp />} />
-        <Route exact path="/counter-redux" element={<ReduxApp />} />
-        <Route exact path="/counter-mobx" element={<MobXApp />} />
-      </Routes>
+      <Suspense fallback={<p>Loading..</p>}>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/articles" element={<Articles />} />
+          <Route exact path="/counter" element={<StateApp />} />
+          <Route exact path="/counter-context" element={<ContextApp />} />
+          <Route exact path="/counter-recoil" element={<RecoilApp />} />
+          <Route exact path="/counter-redux" element={<ReduxApp />} />
+          <Route exact path="/counter-mobx" element={<MobXApp />} />
+          <Route exact path="/code-splitting" element={<AboutApp />} />
+        </Routes>
+      </Suspense>
       {/*       </Router> */}
     </>
   );
